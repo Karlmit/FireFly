4603772b9732e8700f85575eaf2d111ea82b1ede
cakelake <k.almqvist.93@gmail.com>
2014-01-27 14:22:35 +0100

    updated EntityList

--- FireFly/FireFly/EntityList.h
+++ FireFly/FireFly/EntityList.h
@@ -1,13 +1,23 @@
 #pragma once
+#include <list>
+#include "Entity.h"
+
 class EntityList
 {
 public:
-	static EntityList &getEntityList();
+	static EntityList &getEntityList(); //singleton
+	typedef std::list <Entity*> entityList;
+	entityList listedEntities;
+	void addEntity(Entity *entity);	//adds new entities
+	void updateList();	//deletes "dead" enetities
 
 private:
 	EntityList();
 	~EntityList();
-	void operator=(const EntityList &el);
-	static EntityList eL;
+	void operator=(const EntityList &eL);	//singleton
+	static EntityList eL;	//singleton
+	typedef std::list <Entity*> tempList; //stores "alive" entities
+	tempList tempEntities;	//used for deleting killed entities
+
 };
 
