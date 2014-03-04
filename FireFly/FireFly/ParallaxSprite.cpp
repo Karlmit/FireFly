99dd1e71b336c72de55a79455985377369035ea1
Joacim Möller <joacim.moller@gmail.com>
2014-03-04 14:26:05 +0100

    Gjorde om telefonsvararen

--- FireFly/FireFly/ParallaxSprite.cpp
+++ FireFly/FireFly/ParallaxSprite.cpp
@@ -18,21 +18,6 @@ ParallaxSprite::ParallaxSprite(string textureID ,sf::Vector2f position)
 
 void ParallaxSprite::updateEntity(sf::Time dt) 
 {
-	// Temp
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-		mPos.x -= 10.f;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-		mPos.x += 10.f;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
-		mPos.y -= 10.f;
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
-		mPos.y += 10.f;
-
-	static int count = 0;
-	if (count == 0)
-		cout << mPos.x << "," << mPos.y << endl;
-	count = (count + 1) % 100;
-
 
 	sf::Vector2f pos;
 	pos.x = mPos.x -  Camera::currentCamera().getPosition().x * mSpeedFactor;
